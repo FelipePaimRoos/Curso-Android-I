caaa6851705ae7222d5a349a1e684972a7a32211
Felipe Paim Roos <felipepaimroos@gmail.com>
2024-04-04 22:32:51 -0300

    Iniciando a implementação de layout de lista

--- app/src/main/java/com/example/agenda/MainActivity.java
+++ app/src/main/java/com/example/agenda/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.agenda;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
@@ -13,8 +16,15 @@ public class MainActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        List<String> alunos = new ArrayList<>(Arrays.asList("Felipe", "Bruno", "Diogo"));
 
+        TextView aluno1 = findViewById(R.id.textView7);
+        TextView aluno2 = findViewById(R.id.textView8);
+        //TextView aluno3 = findViewById(R.id.textView9);
 
+        aluno1.setText(alunos.get(0));
+        aluno2.setText(alunos.get(1));
+        //aluno3.setText(alunos.get(2));
         /*
         Não é uma boa prática, quanto menos responsabilidade pra ela melhor.
         Melhor criar um arquivos estático pra isso.

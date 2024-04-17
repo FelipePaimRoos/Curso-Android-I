4be3505daf923b1ced8726f60f75e5c9909a9922
Felipe Paim Roos <felipepaimroos@gmail.com>
2024-04-17 19:47:21 -0300

    criando aluno

--- app/src/main/java/com/example/agenda/ui/ActivityFormularioAluno.java
+++ app/src/main/java/com/example/agenda/ui/ActivityFormularioAluno.java
@@ -1,6 +1,10 @@
 package com.example.agenda.ui;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,7 +19,26 @@ public class ActivityFormularioAluno extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_formulario_aluno);
+        final EditText campoNome = findViewById(R.id.activity_formulario_aluno_nome);
+        final EditText campoTelefone = findViewById(R.id.activity_formulario_aluno_telefone);
+        final EditText campoEmail = findViewById(R.id.activity_formulario_aluno_email);
+
+
+
+        Button botaoSalvar = findViewById(R.id.activity_formulario_aluno_botao_salvar);
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+               // Toast.makeText(ActivityFormularioAluno.this, "Cliquei no botão salvar", Toast.LENGTH_SHORT).show();
+                String nome = campoNome.getText().toString();
+                String telefone = campoTelefone.getText().toString();
+                String email = campoEmail.getText().toString();
+
+                Aluno aluno = new Aluno(nome, telefone, email);
+                Toast.makeText(ActivityFormularioAluno.this, " "+aluno.getNome() + " - "+aluno.getTelefone()+ " - "+ aluno.getEmail(), Toast.LENGTH_SHORT).show();
+            }
+            });
+
     }
 }
